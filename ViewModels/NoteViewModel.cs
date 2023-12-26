8e61482ba041bf065835ded55634ed6fdf3a3277
Juan Andres Alvarez Pullas <juan.alvarez.pullas@udla.edu.ec>
2023-12-26 10:51:01 -0500

    Cambios en espacios de nombres de varios  archivos en ViewModels y Views.

--- ViewModels/NoteViewModel.cs
+++ ViewModels/NoteViewModel.cs
@@ -3,10 +3,81 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Windows.Input;
 
 namespace Alvarez_AppApuntes.ViewModels
 {
-    internal class NoteViewModel
+    internal class NoteViewModel:ObservableObject, IQueryAttributable
     {
+        private Models.Note _note;
+        public string Text
+        {
+            get => _note.Texto_Alvarez;
+            set
+            {
+                if (_note.Texto_Alvarez != value)
+                {
+                    _note.Texto_Alvarez = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public DateTime Date => _note.Fecha_Alvarez;
+
+        public string Identifier => _note.Filename_Alvarez;
+
+        public ICommand SaveCommand { get; private set; }
+        public ICommand DeleteCommand { get; private set; }
+
+        public NoteViewModel()
+        {
+            _note = new Models.Note();
+            SaveCommand = new AsyncRelayCommand(Save);
+            DeleteCommand = new AsyncRelayCommand(Delete);
+        }
+
+        public NoteViewModel(Models.Note note)
+        {
+            _note = note;
+            SaveCommand = new AsyncRelayCommand(Save);
+            DeleteCommand = new AsyncRelayCommand(Delete);
+        }
+
+        private async Task Save()
+        {
+            _note.Fecha_Alvarez = DateTime.Now;
+            _note.Save();
+            await Shell.Current.GoToAsync($"..?saved={_note.Filename_Alvarez}");
+        }
+
+        private async Task Delete()
+        {
+            _note.Delete();
+            await Shell.Current.GoToAsync($"..?deleted={_note.Filename_Alvarez}");
+        }
+
+        void IQueryAttributable.ApplyQueryAttributes(IDictionary<string, object> query)
+        {
+            if (query.ContainsKey("load"))
+            {
+                _note = Models.Note.Load(query["load"].ToString());
+                RefreshProperties();
+            }
+        }
+
+        public void Reload()
+        {
+            _note = Models.Note.Load(_note.Filename_Alvarez);
+            RefreshProperties();
+        }
+
+        private void RefreshProperties()
+        {
+            OnPropertyChanged(nameof(Text));
+            OnPropertyChanged(nameof(Date));
+        }
     }
 }
