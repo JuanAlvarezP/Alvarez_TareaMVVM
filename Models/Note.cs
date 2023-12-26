d342cc8f447e863a0c9497c40fd3e4ee1b1dcd41
Juan Andres Alvarez Pullas <juan.alvarez.pullas@udla.edu.ec>
2023-12-26 10:31:25 -0500

    Creacion de carpeta ViewModels y sus respectivas clases

--- Models/Note.cs
+++ Models/Note.cs
@@ -11,5 +11,56 @@ namespace Alvarez_AppApuntes.Models
         public string Filename_Alvarez { get; set; }
         public string Texto_Alvarez { get; set; }
         public DateTime Fecha_Alvarez { get; set; }
+
+        public Note()
+        {
+            Filename_Alvarez = $"{Path.GetRandomFileName()}.notes.txt";
+            Fecha_Alvarez = DateTime.Now;
+            Texto_Alvarez = "";
+        }
+
+        public void Save() =>
+    File.WriteAllText(System.IO.Path.Combine(FileSystem.AppDataDirectory, Filename_Alvarez), Texto_Alvarez);
+
+        public void Delete() =>
+            File.Delete(System.IO.Path.Combine(FileSystem.AppDataDirectory, Filename_Alvarez));
+
+        public static Note Load(string filename)
+        {
+            filename = System.IO.Path.Combine(FileSystem.AppDataDirectory, filename);
+
+            if (!File.Exists(filename))
+                throw new FileNotFoundException("Unable to find file on local storage.", filename);
+
+            return
+                new()
+                {
+                    Filename_Alvarez = Path.GetFileName(filename),
+                    Texto_Alvarez = File.ReadAllText(filename),
+                    Fecha_Alvarez = File.GetLastWriteTime(filename)
+                };
+        }
+
+        public static IEnumerable<Note> LoadAll()
+        {
+            // Get the folder where the notes are stored.
+            string appDataPath = FileSystem.AppDataDirectory;
+
+            // Use Linq extensions to load the *.notes.txt files.
+            return Directory
+
+                    // Select the file names from the directory
+                    .EnumerateFiles(appDataPath, "*.notes.txt")
+
+                    // Each file name is used to load a note
+                    .Select(filename => Note.Load(Path.GetFileName(filename)))
+
+                    // With the final collection of notes, order them by date
+                    .OrderByDescending(note => note.Fecha_Alvarez);
+        }
+
+        
     }
+
+    
 }
